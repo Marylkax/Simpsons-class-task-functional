7700680b3f45976d6899f16a2446ea50a184c5b9
Marylkax <marylkagriffiths@gmail.com>
2023-06-06 14:00:55 +0100

    modified reset function

--- src/components/Delete.jsx
+++ src/components/Delete.jsx
@@ -3,10 +3,11 @@ import React, { Component } from "react";
 class Delete extends Component {
   render() {
     const { onDelete, id } = this.props;
-
+// console.log(this.props); chechking that it has recieved the prop onDelete
     return (
       <div>
-        <button onClick={() => onDelete(id)}>Delete</button>
+        <button onClick={() => onDelete(id)}>Delete</button> 
+        {/* a callback: functions passed through functions. Can call function in the parent, sends data. Uses unique id */}
       </div>
     );
   }
