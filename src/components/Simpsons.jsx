1d053ed7323a4c732489daec8876863c13c14992
Marylkax <marylkagriffiths@gmail.com>
2023-06-12 18:03:18 +0100

    Update Simpsons.jsx

--- src/components/Simpsons.jsx
+++ src/components/Simpsons.jsx
@@ -2,64 +2,41 @@ import React, { Component } from "react";
 import Character from "./Character";
 import Controls from "./Controls";
 
-// don't need to import React when using functional?   
+// don't need to import React when using functional?
 
-const Simpsons = (props) =>{ 
-  
-    const { simpsons, onDelete, onLikeToggle, onSearchInput, onAlphabetList, onReset, searchInput, alphabetList} = props;
+const Simpsons = (props) => {
+  const {
+    simpsons,
+    onDelete,
+    onLikeToggle,
+    onSearchInput,
+    onAlphabetList,
+    onReset,
+    search,
+    alphabetList,
+  } = props;
 
-    return (
-      <>
-       <Controls
+  return (
+    <>
+      <Controls
         onSearchInput={onSearchInput}
         onAlphabetList={onAlphabetList}
         onReset={onReset}
-        searchInput={searchInput}
+        search={search}
         alphabetList={alphabetList}
-        />
-        {simpsons.map((item, index) => {
-          return (
-            <Character
-              item={item}
-              key={item.id}
-              onDelete={onDelete} //passing down to Character component
-              onLikeToggle={onLikeToggle}
-            />
-          );
-        })}
-      </>
-    );
-
-}
-
-
-// class Simpsons extends Component {
-//   render() {
-//     const { simpsons, onDelete, onLikeToggle, onSearchInput, onAlphabetList, onReset, searchInput, alphabetList} = this.props;
-
-//     return (
-//       <>
-//        <Controls
-//         onSearchInput={onSearchInput}
-//         onAlphabetList={onAlphabetList}
-//         onReset={onReset}
-//         searchInput={searchInput}
-//         alphabetList={alphabetList}
-//         />
-//         {simpsons.map((item, index) => {
-//           return (
-//             <Character
-//               item={item}
-//               key={item.id}
-//               onDelete={onDelete} //passing down to Character component
-//               onLikeToggle={onLikeToggle}
-//             />
-//           );
-//         })}
-//       </>
-//     );
-//   }
-// }
-// no state or lifecycle methods, so this is able to be converted to functional way. Able to have mix of class based and functional components
+      />
+      {simpsons.map((item, index) => {
+        return (
+          <Character
+            item={item}
+            key={item.id}
+            onDelete={onDelete} //passing down to Character component
+            onLikeToggle={onLikeToggle}
+          />
+        );
+      })}
+    </>
+  );
+};
 
 export default Simpsons;
