1890a3ded45f35c3d85e69e722ef77fd28db8e96
Marylkax <marylkagriffiths@gmail.com>
2023-06-12 18:05:34 +0100

    Update Controls.jsx
    search input passed to component
    sorting and filtering, with reset button added

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -1,36 +1,25 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
 
 const Controls = (props) => {
-    const {onSearchInput, onAlphabetList, onReset, searchInput, alphabetList} = props;
-        return (
-        <>
-        <input id="inputbox" onInput={onSearchInput} type="text" value={searchInput}/>
-        <select id="listbox" onChange={onAlphabetList} value={alphabetList}>
+  const { onSearchInput, onAlphabetList, onReset, search, alphabetList } =
+    props;
+  return (
+    <>
+      <input
+        id="inputbox"
+        onInput={onSearchInput}
+        type="text"
+        search={search}
+      />
+      <select id="listbox" onChange={onAlphabetList} value={alphabetList}>
         <option value=""></option>
         <option value="asc"> Asc </option>
         <option value="desc">Desc</option>
-        </select>
-        <button onClick={onReset}> Reset</button>
-        </>
-        );
-    }
-// class Controls extends Component {
-   
-//     render() { 
-//         const {onSearchInput, onAlphabetList, onReset, searchInput, alphabetList} = this.props;
-//         return (
-//         <>
-//         <input id="inputbox" onInput={onSearchInput} type="text" value={searchInput}/>
-//         <select id="listbox" onChange={onAlphabetList} value={alphabetList}>
-//         <option value=""></option>
-//         <option value="asc"> Asc </option>
-//         <option value="desc">Desc</option>
-//         </select>
-//         <button onClick={onReset}> Reset</button>
-//         </>
-//         );
-//     }
-// }
- 
+      </select>
+      <button onClick={onReset}> Reset</button>
+    </>
+  );
+};
+
+
 export default Controls;
